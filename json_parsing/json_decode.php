61f129d3f7af6b67888287c08475cda9d211415e
sushantN062 <78634805+sushantN062@users.noreply.github.com>
2022-05-23 23:05:40 +0545

    Delete json_parsing directory

--- json_parsing/json_decode.php
+++
@@ -1,4 +0,0 @@
-<?php
-$json ='{"Peter":65, "Harry":80,"John:78, "Clark":90}';
-var_dump(json_decode($json));
-?>
--- json_parsing/json_encode_example.php
+++
@@ -1,8 +0,0 @@
-<?php
-$marks = array("Peter"=>65, "Harry"=>80, "John"=>78, "Clark"=>90);
-
-$colors = array("Red", "Green", "Blue", "Yellow", "Orange");
-echo json_encode($marks);
-echo json_encode($marks["Peter"]);
-echo json_encode(colors[2])
-?>
